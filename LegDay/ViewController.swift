eaf12577a00734a7d0d408da800d3f9982ae5195
juger rash <jugerrash@jugers-Mac.local>
2019-09-11 23:43:34 -0700

    LegDay app has been created

---
+++ LegDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LegDay
+//
+//  Created by juger rash on 11.09.19.
+//  Copyright © 2019 juger rash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
